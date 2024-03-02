b81f903d0f5d2b3f9e16c412287e93d9f5c399cb
Tareq.z <tzobeyd65@gmail.com>
2024-03-02 15:42:17 +0330

    feat: fix previous commit : add cli/menu to select vehicles by players

--- main.php
+++ main.php
@@ -16,6 +16,7 @@ foreach ($vehiclesData as $index => $vehicle) {
     $choices[] = $vehicle['name'];
 }
 
-// Create vehicles objects based on player selections
 $selectedVehicleIndex1 = cli\menu($choices, false, "Player 1, choose your vehicle:");
 $selectedVehicleIndex2 = cli\menu($choices, false, "Player 2, choose your vehicle:");
+
+
