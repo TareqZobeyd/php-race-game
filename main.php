82692f94326ec556dda96e3758c85809c310eb5d
Tareq.z <tzobeyd65@gmail.com>
2024-03-02 15:26:02 +0330

    feat: Load vehicles data from JSON file

--- main.php
+++ main.php
@@ -3,3 +3,6 @@
 require_once 'Vehicle.php';
 require_once 'Race.php';
 require_once './php-cli-tools/lib/cli/cli.php';
+
+// Load vehicles data from JSON file
+$vehiclesData = json_decode(file_get_contents('vehicles.json'), true);
