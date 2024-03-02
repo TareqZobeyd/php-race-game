b141497beaec975b93fa94c0d952bae422694410
Tareq.z <tzobeyd65@gmail.com>
2024-03-02 11:07:00 +0330

    feat: add construct method to vehicle class

--- vehicle.php
+++ vehicle.php
@@ -6,4 +6,10 @@ class vehicle
     private $name;
     private $speed;
 
+    public function __construct($name, $speed)
+    {
+        $this->name = $name;
+        $this->speed = $speed;
+    }
+
 }
