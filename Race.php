5479ccfe091af5c3ea205103f9fb0d6c04e89a85
Tareq.z <tzobeyd65@gmail.com>
2024-03-02 11:17:43 +0330

    feat: add start method to race class

--- Race.php
+++ Race.php
@@ -5,10 +5,19 @@ class Race
 
     private $player1Vehicle;
     private $player2Vehicle;
+
     public function __construct($player1Vehicle, $player2Vehicle)
     {
         $this->player1Vehicle = $player1Vehicle;
         $this->player2Vehicle = $player2Vehicle;
     }
 
+    public function start($distance)
+    {
+        $player1Time = $this->player1Vehicle->calculateTime($distance);
+        $player2Time = $this->player2Vehicle->calculateTime($distance);
+
+        return [$player1Time, $player2Time];
+
+    }
 }
