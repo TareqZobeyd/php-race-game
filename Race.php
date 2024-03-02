2bac33b9ae73178712c936d9de9986ed4de6211f
Tareq.z <tzobeyd65@gmail.com>
2024-03-02 11:15:40 +0330

    feat: add instance and construct method for playe1 vehicle and second

--- Race.php
+++ Race.php
@@ -3,4 +3,12 @@
 class Race
 {
 
+    private $player1Vehicle;
+    private $player2Vehicle;
+    public function __construct($player1Vehicle, $player2Vehicle)
+    {
+        $this->player1Vehicle = $player1Vehicle;
+        $this->player2Vehicle = $player2Vehicle;
+    }
+
 }
