c89f57fa6b0e31e41a3c7e21de2df6b12f2ce4bb
Tareq.z <tzobeyd65@gmail.com>
2024-03-03 11:31:06 +0330

    feat: create class racing game for the single responsibility prenciple

---
+++ RacingGame.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Vehicle.php';
+require_once 'Race.php';
+require_once './php-cli-tools/lib/cli/cli.php';
+require_once './php-cli-tools/lib/cli/streams.php';
+require_once './php-cli-tools/lib/cli/colors.php';
+require_once './php-cli-tools/lib/cli/Table.php';
+require_once './php-cli-tools/lib/cli/Shell.php';
+require_once './php-cli-tools/lib/cli/table/Renderer.php';
+require_once './php-cli-tools/lib/cli/table/Ascii.php';
+class RacingGame
+{
+
+}
