be1ebbe9e50786b8ff6179da62bd0d143d6420dd
Tareq.z <tzobeyd65@gmail.com>
2024-03-03 11:19:22 +0330

    feat: add method for convert speed in Km/h

--- Vehicle.php
+++ Vehicle.php
@@ -19,9 +19,23 @@ class vehicle
         return $this->name;
     }
 
-    public function getSpeed()
+    public function getMaxSpeed()
     {
-        return $this->speed;
+        return $this->maxSpeed;
+    }
+
+    // Convert speed to a common unit (Km/h) for calculation
+    public function getSpeedInKmPerHour()
+    {
+        if ($this->unit === 'Km/h') {
+            return $this->maxSpeed;
+        } elseif ($this->unit === 'knots') {
+            // 1 knot = 1.852 Km/h
+            return $this->maxSpeed * 1.852;
+        } elseif ($this->unit === 'Kts') {
+            // 1 Knot (Kts) = 1.852 Kilometer Per Hour (Km/h)
+            return $this->maxSpeed * 1.852;
+        }
     }
 
     public function calculateTime($distance)
